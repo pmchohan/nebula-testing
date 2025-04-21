7ad45e1d92ec535d29d265821a3a3a088de8d067
pmcutorials <pmcutorials@gmail.com>
2025-04-21 15:17:06 +0000

    bug fixed

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 dir_path = os.path.join(os.path.dirname(__file__), 'data')
 file_path = os.path.join(dir_path, 'example.txt')
 
+os.makedirs(dir_path, exist_ok=True)
+
 with open(file_path, 'w') as file:
     file.write("Hello, World!")
     file.write("\nThis is a test file.")
